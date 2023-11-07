224bd282f2c7160bcf263bcac1f1bb9982854c69
soheb-funtune <soheb.sayyad@fyntune.com>
2023-11-07 10:30:27 -0800

    login page ui

---
+++ src/Pages/LoginSignUp/LoginSignUp.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import "./LoginSignUp.css";
+
+const LoginSignUp = () => {
+  return (
+    <div className="login-signup">
+      <div className="login-signup-container">
+        <h1>Sign Up</h1>
+        <div className="login-signup-fields">
+          <input type="text" placeholder="Enter Name" />
+          <input type="email" placeholder="Enter Email" />
+          <input type="password" placeholder="Enter Password" />
+        </div>
+        <button className="btn">Continue</button>
+        <p className="aleady-have-account">
+          Already have an Account <span>Login</span>
+        </p>
+        <div className="login-signup-agree">
+          <input type="checkbox" />
+          <p>By continuing, i agree to the terms of use & privacy policy.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginSignUp;
